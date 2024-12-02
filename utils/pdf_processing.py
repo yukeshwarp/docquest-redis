5e8662871528a52bfb6d0e5490627c2b5ccf499d
yukeshwarp <Yukeshwarp@docu3c.com>
2024-12-03 00:24:05 +0530

    Update pdf_processing.py

--- utils/pdf_processing.py
+++ utils/pdf_processing.py
@@ -150,10 +150,9 @@ def process_pdf_pages(uploaded_file, first_file=False):
             for page_number in range(total_pages):
                 page = pdf_document.load_page(page_number)
                 full_text += page.get_text("text").strip() + " "
-                # if count_tokens(full_text)>180000 or total_pages>500:
-                #     return ""
-            logging.error(count_tokens(full_text))
-            return ""
+                st.session_state.RAW_TOKEN += count_tokens(page.get_text("text").strip() + " ")
+                if count_tokens(full_text)>2500000 or st.session_state.RAW_TOKEN>250000 or total_pages>470:
+                    return ""
             first_200_words = " ".join(full_text.split()[:200])
             generated_system_prompt = generate_system_prompt(first_200_words)
 
