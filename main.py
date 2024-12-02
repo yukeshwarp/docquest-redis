065b1a9b51b0d2e9f4e418214bc190b6bee0abc2
yukeshwarp <Yukeshwarp@docu3c.com>
2024-12-02 22:03:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -190,6 +190,7 @@ with st.sidebar:
                         document_data = process_pdf_task(uploaded_file, first_file=(i == 0))
                         if document_data is "":
                             st.warning('Document is too big!', icon="⚠️")
+                            st.stop()
                         st.session_state.doc_token += count_tokens(str(document_data))
                         if st.session_state.doc_token>400000:
                             st.warning('Document is too large to query, results may be inaccurate. Consider uploading smaller document.', icon="⚠️")
